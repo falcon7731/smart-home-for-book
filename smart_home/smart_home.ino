5c6ae3437954fe7326bc8c078fe69a9b5c549e7b
falcon7731 <mehregan.zare7731@gmail.com>
2019-09-21 23:55:57 +0430

    fixed issues

--- smart_home/smart_home.ino
+++ smart_home/smart_home.ino
@@ -1,13 +1,10 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-//I2C pins declaration
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
 //requiers libraries
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 // Smart garden pins
 int pump_pin_number = 13;
 int soil_water_level_pin = 12;
-int fan_pin_number = 11
+int fan_pin_number = 11;
 int LM35_pin_number = A0;
 int UltraSonic_Echo_pin_number = 10;
 int UltraSonic_Trig_pin_number = 9;
@@ -19,7 +16,9 @@ int Buzzer_pin_number = 6;
 int SDA_pin_number = A4;
 int Scl_pin_numberr = A5;
 //General variables
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C lcd(0x27, 16,2);
+float Temperature_C = 0.00;
+bool soil_water_level = LOW;
 void setup() {
   //setting up pinmodes
   pinMode(pump_pin_number , OUTPUT);
@@ -29,10 +28,31 @@ void setup() {
   pinMode(ReedSwitch_pin_number , INPUT);
   pinMode(MQ5_pin_number , INPUT);
   pinMode(Buzzer_pin_number , OUTPUT);
+  //starting the lcd
+  lcd.begin(16, 2); //Defining 16 columns and 2 rows of lcd display
+  lcd.backlight();//To Power ON the back light
+  //starting serial com
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //gathering all the data
 
+
+}
+
+void Get_temprature() {
+  int val = analogRead(LM35_pin_number);
+  float mv = ( val / 1024.0) * 5000;
+  float cel = mv / 10;
+  Serial.print("TEMPRATURE = ");
+  Serial.print(cel);
+  Serial.print("*C");
+  Serial.println();
+  Temperature_C = cel;
+}
+
+void soil_status(){
+  
 }
